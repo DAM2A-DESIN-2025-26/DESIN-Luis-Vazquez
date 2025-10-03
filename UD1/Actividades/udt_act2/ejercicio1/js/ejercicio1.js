3c22a4ae90dc779a2b77006bb70abe2cb6fe6cb0
luisvg42 <luisvg42@educastur.es>
2025-10-03 17:08:14 +0200

    hasta ejercicio2 a medias

---
+++ UD1/Actividades/udt_act2/ejercicio1/js/ejercicio1.js
@@ -0,0 +1,71 @@
+/*Crea un programa que pida al usuario su nombre y apellidos y muestre:
+• El tamaño del nombre más los apellidos (sin contar espacios).
+• La cadena en minúsculas y en mayúsculas.
+• Que divida el nombre y los apellidos y los muestre en 3 líneas, donde ponga
+Nombre:
+Apellido 1:
+Apellido 2:
+o Una propuesta de nombre de usuario, compuesto por el nombre, la inicial del primer
+apellido y la inicial del segundo: ej. Para Javier Gonzalez Pisano sería javiergp.
+Implementa esta funcionalidad mediante una función anónima.
+
+o Una propuesta de nombre de usuario compuesto por las dos primeras letras del
+nombre y de los dos apellidos: ej. jagopi. Implementa esta funcionalidad mediante
+una función flecha. */
+
+var nombreApellidos = prompt("Introduce tu nombre y apellidos: ")
+
+function mostrarDatos(nombreApellidos) {
+
+    let nombreApellidosSinEspacios = nombreApellidos.replace(" ", "");
+    let tamaño = nombreApellidosSinEspacios.length;
+    let mayus = nombreApellidos.toUpperCase();
+    let minus = nombreApellidos.toLowerCase();
+
+    let porPartes = nombreApellidos.split(' ');
+    let arr = ["nombre: ", "apellido1: ", "apellido2: "]
+    console.log("Tamaño de " + nombreApellidosSinEspacios + ": " + tamaño);
+    console.log("En mayusculas: " + mayus);
+    console.log("En minusculas: " + minus);
+
+    for (let i = 0; i < porPartes.length; i++) {
+        console.log(arr[i] + porPartes[i])
+
+    }
+
+}
+const nombreUser = function (nombreApellidos) {
+
+    var arr = nombreApellidos.split(' ');
+    let nombreUsuario = "";
+    for (let i = 0; i < arr.length; i++) {
+
+        if (i == 0) {
+            nombreUsuario += arr[i]
+        } else {
+            nombreUsuario += arr[i].substr(0, 1)
+        }
+    }
+
+    console.log(nombreUsuario)
+    return nombreUsuario;
+
+}
+
+var nombreUserDos = (nombreApellidos) => {
+    var arr = nombreApellidos.split(' ');
+    var nombreUser = "";
+    for (let i = 0; i < arr.length; i++) {
+        nombreUser += arr[i].substr(0, 2)
+
+    }
+    console.log(nombreUser)
+    return nombreUser;
+
+}
+
+mostrarDatos(nombreApellidos);
+const nombreUsuario = nombreUser(nombreApellidos);
+
+var usuario2 = nombreUserDos(nombreApellidos);
+
---
+++ UD1/Actividades/udt_act2/ejercicio2/js/ejercicio2.js
@@ -0,0 +1,20 @@
+var diaCumple = 12
+var mesCumple = 4
+
+function infoCumple(diaCumple, mesCumple) {
+
+    var fechaHoy = new Date();
+    var fechaCumple = new Date(2025, 4, 12);
+
+    //esto me devuelve la diferencia en milisegundos (creo)
+    var diferencia = fechaCumple.getTime() - fechaHoy.getTime()
+    var diferenciaDias = Math.round(diferencia / (1000 * 60 * 60 * 24))
+
+    console.log(fechaHoy)
+    console.log(fechaCumple)
+    console.log(diferenciaDias)
+
+    return {}
+}
+
+infoCumple(diaCumple, mesCumple)
