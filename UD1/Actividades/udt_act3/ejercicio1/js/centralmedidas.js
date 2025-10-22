eeb61da5a9557fa5ca2620e17265b403c751f8e8
luisvg42 <luisvg42@educastur.es>
2025-10-22 19:59:25 +0200

    hacer actividad3

---
+++ UD1/Actividades/udt_act3/ejercicio1/js/centralmedidas.js
@@ -0,0 +1,42 @@
+class CentralMedidas {
+   
+
+    #medidas;
+
+    constructor(medidas) {
+        this.#medidas = [];
+    }
+
+    get medidas() {
+        return this.#medidas;
+    }
+    
+    set medidas(value) {
+       this.#medidas = value ;
+    }
+
+    insertaMedidas(ciudad,valores){
+
+    }
+
+    insertaAleatorios(ciudad){
+
+    }
+
+    mediaMedidas(ciudad){
+
+    }
+
+    mediaMedidasTotal(){
+
+    }
+    
+    eliminaCiudad(ciudad){
+
+    }
+
+    toConsole(){
+
+    }
+
+}
