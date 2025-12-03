7745baf715279d65c283cb90cd4f85a87acc5508
luisvg42 <luisvg42@educastur.es>
2025-12-03 15:58:10 +0100

    dragonball al dia

--- UD2/Angular/prueba/src/app/pages/dragon-ball/dragon-ball.ts
+++ UD2/Angular/prueba/src/app/pages/dragon-ball/dragon-ball.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 
-export interface Personaje {
+interface Personaje {
   id: number;
   name: String;
   power: number;
@@ -13,10 +13,29 @@ export interface Personaje {
   styleUrl: './dragon-ball.scss',
 })
 export class DragonBall {
-  personajes = signal <Personaje[]>([
+  name = signal('');
+  power = signal(0);
+
+  pintarValores() {
+    // console.log(this.name(), this.power());
+    const newPersonaje: Personaje = {
+      id: this.personajes.length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.personajes.update((personajes) => [...personajes, newPersonaje]);
+    this.limpiarCampos();
+  }
+
+  limpiarCampos() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
+  personajes = signal<Personaje[]>([
     { id: 1, name: 'Goku', power: 3000 },
     { id: 2, name: 'Krillin', power: 100 },
     { id: 3, name: 'Piccolo', power: 2000 },
   ]);
-
 }
